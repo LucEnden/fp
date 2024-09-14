39a970bedb0c9e6d5f0b37e841f7f98232cd1923
Enden,Luc L. van den <i453142@FontysICT.nl>
2024-09-14 15:54:25 +0200

    Implemented Week 1 assigments
    Tested with all test cases from PDF, all outputs are as expected

--- src/Ceaser.elm
+++ src/Ceaser.elm
@@ -1,14 +1,47 @@
 module Ceaser exposing (..)
 
+-- ASCII characters that are important to the implementations of encode/decode:
+-- A / 65
+-- ...
+-- Z / 90
+-- a / 97
+-- ...
+-- z / 122
+
+-- Taken from the excersise PDF:
+-- "Note that lower case letters remain lower case, and upper case letters remain upper case."
+
 encode: Int -> Char -> Char
 encode offset original = 
-    -- A / 65
-    -- ...
-    -- Z / 90
-    -- a / 97
-    -- ...
-    -- z / 122
 
-    -- Taken from the excersise PDF:
-    -- "Note that lower case letters remain lower case, and upper case letters remain upper case."
-    original
+    let
+        o = Char.toCode original
+        i = modBy 26 offset
+
+        (min, max) =
+            if o >= 65 && o <= 90 then
+                (65, 90)
+            else
+                (97, 122)
+    in
+
+    -- with the input 5 'x' we expect the output to be 99
+    -- Debug.log (Debug.toString o) -- o = 120
+    -- Debug.log (Debug.toString i) -- i = 5 mod 26 = 5
+    -- Debug.log (Debug.toString min) -- min is 97
+    -- Debug.log (Debug.toString max) -- max is 122
+    -- Debug.log (Debug.toString (o + i)) -- o + i = 125
+    -- Debug.log (Debug.toString (o + i - max + min)) -- 125 - 90 + 65 = 100
+    -- we end with a - 1 to make the range inclusive
+    -- we only need to map to range (i.e. subtract max and add min - 1) o + i > max
+    if (o + i) > max then
+        Char.fromCode (o + i - max + min - 1)
+    else
+        Char.fromCode (o + i)
+
+decode: Int -> Char -> Char
+decode offset original =
+    if offset < 0 then
+        encode offset original
+    else
+        encode (0 - offset) original
---
+++ src/Pythagoras.elm
@@ -0,0 +1,34 @@
+module Pythagoras exposing (..)
+
+sqr: Int -> Int
+sqr n =
+    n * n
+
+isTriple: Int -> Int -> Int -> Bool
+isTriple a b c =
+    -- Debug.log (Debug.toString (sqr a))
+    -- Debug.log (Debug.toString (sqr b))
+    -- Debug.log (Debug.toString (sqr c))
+
+    if a < 0 || b < 0 || c < 0 then
+        False
+    else
+        (sqr a) + (sqr b) == (sqr c)
+
+leg1 : Int -> Int -> Int
+leg1 x y =
+    (sqr x) - (sqr y)
+leg2 : Int -> Int -> Int
+leg2 x y =
+    2 * x * y
+hyp  : Int -> Int -> Int
+hyp x y =
+    (sqr x) + (sqr y)
+
+pythTriple : (Int, Int) -> (Int, Int, Int)
+pythTriple (x, y) =
+    (leg1 x y, leg2 x y, hyp x y)
+
+isTripleTuple: (Int, Int, Int) -> Bool
+isTripleTuple (x, y, z) =
+    isTriple x y z
